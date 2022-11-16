da65db2c9bbd151b0399f2ba469d5e8dc8993a11
lioGR <liorgrishinnew@gmail.com>
2022-11-16 23:56:19 +0200

    Added Todo

--- Shop.v1/server/API/products/productsCtrl.ts
+++ Shop.v1/server/API/products/productsCtrl.ts
@@ -4,6 +4,8 @@ export function getStoreType(req: express.Request, res: express.Response) {
     try {
         const { storeTypeParam } = req.body;
         if (!storeTypeParam) throw new Error("Couldn't receive storeType from req.body");
+        // add sql query to get all the items with the same type
+        // change name of function and route to productsType to be consistent
         res.send({ storeTypeParam });
     } catch (error) {
         res.status(500).send({ error: error.message })
