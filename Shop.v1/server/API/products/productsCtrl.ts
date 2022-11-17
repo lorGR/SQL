bb1f0df9a63dfdb35ec47856cbe60274cc15a1e4
lioGR <liorgrishinnew@gmail.com>
2022-11-17 10:32:02 +0200

    Added products to DB

--- Shop.v1/server/API/products/productsCtrl.ts
+++ Shop.v1/server/API/products/productsCtrl.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import { connection } from "../../DB/databaseSQL";
 
 export function getStoreType(req: express.Request, res: express.Response) {
     try {
@@ -6,6 +7,15 @@ export function getStoreType(req: express.Request, res: express.Response) {
         if (!storeTypeParam) throw new Error("Couldn't receive storeType from req.body");
         // add sql query to get all the items with the same type
         // change name of function and route to productsType to be consistent
+        const sql = `SELECT DISTINCT name FROM products WHERE type = '${storeTypeParam}'`;
+        connection.query(sql, (error, result) => {
+            try {
+                if(error) throw error;
+                console.log(result);
+            } catch (error) {
+                res.status(500).send({ error: error.message });
+            }
+        });
         res.send({ storeTypeParam });
     } catch (error) {
         res.status(500).send({ error: error.message })
