1ebdec3a59cb85d3b73d6b225b16be0326c489a8
lioGR <liorgrishinnew@gmail.com>
2022-11-20 10:04:57 +0200

    Update Product.tsx

--- Shop.v2/client/src/views/product/Product.tsx
+++ Shop.v2/client/src/views/product/Product.tsx
@@ -3,10 +3,10 @@ import { useParams } from "react-router-dom"
 const Product = () => {
 
     const { product } = useParams();
-    
+
     return (
         <div>{product}</div>
     )
 }
 
-export default Product
+export default Product;
