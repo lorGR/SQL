dcf873c573f01dbfe88f6329432f5b92ebe44307
lioGR <liorgrishinnew@gmail.com>
2022-11-21 16:04:16 +0200

    fixed product form and get information from form

--- Shop.v3/client/src/components/productForm/ProductForm.tsx
+++ Shop.v3/client/src/components/productForm/ProductForm.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react"
+import React, { useEffect } from "react"
 import { useParams } from "react-router-dom"
 import { useAppDispatch, useAppSelector } from "../../app/hooks"
 import { getUserByCookie } from "../../features/user/userAPI"
@@ -13,24 +13,54 @@ interface ProductFormProps {
 
 const ProductForm: React.FC<ProductFormProps> = ({ productInfo, productColors }) => {
     const { storeType } = useParams();
-
+    const { productName } = useParams();
+    
     const user: User = useAppSelector(selectUser);
     const dispatch = useAppDispatch();
 
     useEffect(() => {
         dispatch(getUserByCookie());
-    },[]);
+    }, []);
+
+    const handleAddToCart = (event: React.FC<HTMLFormElement> | any) => {
+        try {
+            event.preventDefault();
+            if (storeType === "mac") {
+                const [productColor, productModel] = [event.target.elements.productColor.value, event.target.elements.productModel.value];
+                console.log(productColor, productModel);
+            } else if (storeType === "iphone" || storeType === "ipad" || storeType === "apple_tv") {
+                const [productColor, storage] = [event.target.elements.productColor.value, event.target.elements.storage.value];
+                console.log(productColor, storage);
+            } else if (storeType === "apple_watch") {
+                const [productColor, screenSize] = [event.target.elements.productColor.value, event.target.elements.screenSize.value];
+                console.log(productColor, screenSize);
+            } else if (storeType === "air_pods" && productName === "AirPods Max") {
+                const productColor = event.target.elements.productColor.value;
+                console.log(productColor);
+            } else {
+                console.log(productName);
+            }
+
+        } catch (error) {
+            console.error(error);
+        }
+    }
 
     return (
         // TODO:
-        // render product img
-        <form>
+        // Render a product header image
+
+        // TODO:
+        // Render product img
+        <form onSubmit={handleAddToCart}>
             {productInfo[0].storage !== null &&
                 productInfo.map((productStorage, idx) => {
                     return (
                         <div key={idx}>
                             <label htmlFor={productStorage.storage!} >{productStorage.storage}</label>
                             <input type="radio" name="storage" id={productStorage.storage!} value={productStorage.storage!} required />
+                            <p>מחיר {productStorage.price} ₪</p>
+                            <p>מחיר באילת {productStorage.price_eilat} ₪</p>
                         </div>
                     );
                 })
@@ -42,6 +72,8 @@ const ProductForm: React.FC<ProductFormProps> = ({ productInfo, productColors })
                         <div key={idx}>
                             <label htmlFor={productScreenSize.screen_size!}>{productScreenSize.screen_size}</label>
                             <input type="radio" name="screenSize" id={productScreenSize.screen_size!} required value={productScreenSize.screen_size!} />
+                            <p>מחיר {productScreenSize.price} ₪</p>
+                            <p>מחיר באילת {productScreenSize.price_eilat} ₪</p>
                         </div>
                     );
                 })
@@ -64,12 +96,14 @@ const ProductForm: React.FC<ProductFormProps> = ({ productInfo, productColors })
                         <div key={idx}>
                             <label htmlFor={productModel.model!}>{productModel.model}</label>
                             <input type="radio" name="productModel" id={productModel.model!} value={productModel.model!} required />
+                            <p>מחיר {productModel.price} ₪</p>
+                            <p>מחיר באילת {productModel.price_eilat} ₪</p>
                         </div>
                     );
                 })
             }
-            {user && <button>הוסף לסל</button>} 
-            {!user && 
+            {user && <button>הוסף לסל</button>}
+            {!user &&
                 <div>
                     <button disabled>הוסף לסל </button>
                     <p>עלייך להירשם או להתחבר בכדי להוסיף מוצר זה לסל הקניות</p>
