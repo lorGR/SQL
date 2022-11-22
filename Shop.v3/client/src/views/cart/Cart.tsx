570a8d2c1a1b51e94205163d723ebff3497f55b1
lioGR <liorgrishinnew@gmail.com>
2022-11-22 12:43:49 +0200

    Rendered product of user

--- Shop.v3/client/src/views/cart/Cart.tsx
+++ Shop.v3/client/src/views/cart/Cart.tsx
@@ -5,13 +5,28 @@ import { useAppDispatch, useAppSelector } from "../../app/hooks"
 import { getUserByCookie } from "../../features/user/userAPI";
 import { selectUser } from "../../features/user/userSlice"
 
+export interface CartProduct {
+    color : string,
+    description: string,
+    display_img: string,
+    model: string | null,
+    name: string,
+    network: string | null,
+    preview_img: string,
+    price: number,
+    price_eilat: number,
+    product_id: number,
+    screen_size: string | null,
+    storage: string | null,
+    type: string
+}
 
 const Cart = () => {
     // TODO: 
     // Check why not getting products when refreshing page
     const user = useAppSelector(selectUser);
 
-    const [userProducts, setUserProducts] = useState();
+    const [userProducts, setUserProducts] = useState<CartProduct[]>();
 
     const getUserProducts = async () => {
         try {
@@ -31,7 +46,11 @@ const Cart = () => {
         getUserProducts();
     }, []);
     // TODO: 
-    // Render products
+    // Create Component of CartProduct
+    // Render button to buy products
+    // Functnions: 
+    // 1. Get the sum of all the products price in user cart
+    // 2. Remove product from cart when user clicks remove
     return (
         <div>
             <h1>סל הקניות</h1>
@@ -40,6 +59,23 @@ const Cart = () => {
                     <Link to="/my-account/registration">הירשם</Link>/
                     <Link to="/my-account">התחבר</Link> בכדי לראות את סל הקניות שלך
                 </p>}
+            {!userProducts &&
+                <p>אין מוצרים בסל קניות</p>
+            }
+            {userProducts && 
+                userProducts.map(userProduct => {
+                    return (
+                        <div>
+                            <h4>{userProduct.name}</h4>
+                            <figure>
+                                <img src={userProduct.display_img} alt={userProduct.name} />
+                            </figure>
+                            <p>מחיר {userProduct.price} ₪</p>
+                            <button>הסר מוצר</button>
+                        </div>
+                    );
+                })
+            }
         </div>
     )
 }
