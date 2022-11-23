d4cdc208a988a6f6e30827c0c474d990c8b213de
lioGR <liorgrishinnew@gmail.com>
2022-11-23 10:22:35 +0200

    styled commercials

--- Shop.v3/client/src/components/commercialCard/CommercialCard.tsx
+++ Shop.v3/client/src/components/commercialCard/CommercialCard.tsx
@@ -12,9 +12,13 @@ const CommercialCard: React.FC<CommercialCardPros> = ({ commHeading, commSubHead
         <Link to={commLinkTo}>
             <div className="commercial-card">
                 <div className="commercial-card__info">
-                    <h3 className="commercial-card__info__title">{commHeading}</h3>
-                    <h4 className="commercial-card__info__subtitle">{commSubHeading}</h4>
-                    <button className="commercial-card__info__btn">הזמינו עכשיו</button>
+                    <div className="commercial-card__info__information">
+                        <p className="commercial-card__info__information__title">{commHeading}</p>
+                        <p className="commercial-card__info__information__subtitle">{commSubHeading}</p>
+                    </div>
+                    <div className="commercial-card__info__btn-container">
+                        <button className="commercial-card__info__btn-container__btn">הזמינו עכשיו</button>
+                    </div>
                 </div>
                 <div className="commercial-card__display">
                     <figure className="commercial-card__display__figure">
