48cc5d8888b4dda5aff38ed957d29d1692b0019b
lioGR <liorgrishinnew@gmail.com>
2022-11-23 07:37:23 +0200

    WIP

--- Shop.v3/client/src/components/navbar/Navbar.tsx
+++ Shop.v3/client/src/components/navbar/Navbar.tsx
@@ -16,17 +16,23 @@ const Navbar = () => {
   
   return (
     <nav className="navbar">
-      <div className="navbar__menu">
-        <Link to="">חיפוש</Link>
-        <Link to="/my-account">החשבון שלי</Link>
-        <Link to="/cart">הסל שלי</Link>
+      
+      <div className="navbar__store-presentation">
+        <Link to="/">
+          <div className="nav__store-presentation__images">
+            <img src="https://www.idigital.co.il/SVTemplate/img/general_new/premium_new.png" alt="apple premium reseller" />
+            <img src="https://www.idigital.co.il/SVTemplate/img/general/iDigital.png" alt="store logo idigital" />
+          </div>
+        </Link>
       </div>
       <div className="navbar__greet-user">
         {!user  && <p>שלום אורח</p>}
         {user && <p>שלום {user.last_name} {user.first_name}</p>}
       </div>
-      <div className="navbar__description">
-        <Link to="/">LOGO</Link>
+      <div className="navbar__menu">
+        {/* <Link to="">חיפוש</Link> */}
+        <Link to="/my-account">החשבון שלי</Link>
+        <Link to="/cart">הסל שלי</Link>
       </div>
     </nav>
   )
--- Shop.v3/client/src/components/storenav/Storenav.tsx
+++ Shop.v3/client/src/components/storenav/Storenav.tsx
@@ -3,13 +3,22 @@ import { Link } from "react-router-dom";
 const Storenav = () => {
   return (
     <div className="storenav">
-        <Link to="/store/mac">Mac</Link>
-        <Link to="/store/iphone">iPhone</Link>
-        <Link to="/store/ipad">iPad</Link>
-        <Link to="/store/apple_watch">Apple Watch</Link>
-        <Link to="/store/air_pods">AirPods</Link>
-        <Link to="/store/apple_tv">Apple TV</Link>
-        {/* <Link to="/store/air_tag">AirTag</Link> */}
+      <Link to="/store/mac">Mac</Link>
+      <Link to="/store/iphone">iPhone</Link>
+      <Link to="/store/ipad">iPad</Link>
+      <Link to="/store/apple_watch">Apple Watch</Link>
+      <Link to="/store/air_pods">AirPods</Link>
+      <Link to="/store/apple_tv">Apple TV</Link>
+      
+      {/* TODO: */}
+      {/* Manage the search bar */}
+      
+      {/* <form action="">
+        <input type="text" name="" id="" placeholder="חיפוש" />
+        <button>מצא לי תוצאות</button>
+      </form> */}
+
+      {/* <Link to="/store/air_tag">AirTag</Link> */}
     </div>
   )
 }
